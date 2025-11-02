1c5abc899d3f19ca400702271c78372d9bc92f3d
Vijay Prakash P J <vijay-prakash.periakonda-jeyaprakash@elli.eco>
2025-11-02 19:24:01 +0100

    Added tests

--- src/core/domain/mod.rs
+++ src/core/domain/mod.rs
@@ -4,7 +4,7 @@ pub enum PetStatus {
     Unavailable,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct PetTag(pub String, pub String);
 
 #[derive(Debug, PartialEq, Eq)]
@@ -14,4 +14,67 @@ pub struct Pet {
     pub status: PetStatus,
     pub category: String,
     pub tags: Vec<PetTag>,
+}
+
+impl Pet {
+    pub fn is_available(self: &Pet) -> bool {
+        self.status == PetStatus::Available
+    }
+
+    pub fn tag(self: &mut Pet, tag: &PetTag) {
+        self.tags.push(tag.to_owned());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+
+    use crate::core::domain::{Pet, PetStatus, PetTag};
+
+    #[rstest]
+    #[case(PetStatus::Available, true)]
+    #[case(PetStatus::Unavailable, false)]
+    fn test_pet_availability(#[case] status: PetStatus, #[case] expected: bool) {
+        let pet = Pet {
+            id: 1,
+            name: "Tommy".to_owned(),
+            status: status,
+            category: "Dog".to_owned(),
+            tags: vec![PetTag("size".to_owned(), "small".to_owned())],
+        };
+
+        assert_eq!(pet.is_available(), expected);
+    }
+
+    #[rstest]
+    pub fn test_add_tag(
+        #[values(vec!(), vec!(PetTag("size".to_owned(), "small".to_owned())))] existing_tags: Vec<
+            PetTag,
+        >,
+    ) {
+        // GIVEN
+        let mut pet = Pet {
+            id: 1,
+            name: "Tommy".to_owned(),
+            status: PetStatus::Available,
+            category: "Dog".to_owned(),
+            tags: existing_tags.clone(),
+        };
+        assert_eq!(pet.tags, existing_tags);
+        let new_tag = PetTag("colour".to_owned(), "brown".to_owned());
+
+        // WHEN
+        pet.tag(&new_tag);
+
+        // THEN
+        assert_eq!(
+            pet.tags,
+            existing_tags
+                .iter()
+                .chain(vec![new_tag].iter())
+                .cloned()
+                .collect::<Vec<PetTag>>()
+        );
+    }
 }
